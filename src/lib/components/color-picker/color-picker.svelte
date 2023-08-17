0bf6dfd6f8a9980de3954b6e95de92df9a3a00bf
Andrew Smith <andrew.smith@wwinc.com>
2023-08-17 13:21:46 -0500

    Got a basic color picker working.

--- src/lib/components/color-picker/color-picker.svelte
+++ src/lib/components/color-picker/color-picker.svelte
@@ -1,22 +1,28 @@
 
 <script>
   import Slider from '$lib/components/color-picker/slider.svelte'
+
+  let red   = 127;
+  let green = 127;
+  let blue  = 127;
 </script>
 
 <div class="mt-5 sliders">
-  <Slider color="Red" value="0"/>
+  <Slider color="Red"   bind:value={red}/>
+  <Slider color="Green" bind:value={green}/>
+  <Slider color="Blue"  bind:value={blue}/>
 </div>
 
-<div class="mt-5 color-box"></div>
+<div class="mt-5 color-box" style="background-color:rgb({red}, {green}, {blue})"></div>
 
 <style>
   .sliders {
-    width: 600px;
+    width: 400px;
   }
 
   .color-box {
-    height:       600px;
-    width:        600px;
+    height:       400px;
+    width:        400px;
     border-style: dashed;
     border-width: 4px;
     border-color: black;
