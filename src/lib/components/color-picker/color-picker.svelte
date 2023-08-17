f14f49b02e54b3b1f5645bb37637becb14682e09
Andrew Smith <andrew.smith@wwinc.com>
2023-08-17 13:00:41 -0500

    Added a dashed border around the color picker.

---
+++ src/lib/components/color-picker/color-picker.svelte
@@ -0,0 +1,11 @@
+<div class="mt-5"></div>
+
+<style>
+  div {
+    height:       400px;
+    width:        400px;
+    border-style: dashed;
+    border-width: 4px;
+    border-color: black;
+  }
+</style>
--- src/routes/color-picker/+page.svelte
+++ src/routes/color-picker/+page.svelte
@@ -1,1 +1,6 @@
+<script>
+  import ColorPicker from '$lib/components/color-picker/color-picker.svelte';
+</script>
 <h1>Color Picker</h1>
+
+<ColorPicker></ColorPicker>
