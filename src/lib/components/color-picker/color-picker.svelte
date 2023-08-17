0bcdc129d1cde8da41298b03f1497bcf7d40e517
Andrew Smith <andrew.smith@wwinc.com>
2023-08-17 13:25:09 -0500

    Finished the color picker.

--- src/lib/components/color-picker/color-picker.svelte
+++ src/lib/components/color-picker/color-picker.svelte
@@ -1,10 +1,23 @@
 
-<script>
+<script lang="ts">
   import Slider from '$lib/components/color-picker/slider.svelte'
 
   let red   = 127;
   let green = 127;
   let blue  = 127;
+
+  function rgbToHex(red: number, green: number, blue: number): string {
+    const toHex = (value: number): string => {
+      const hex = value.toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    };
+
+    if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255) {
+      throw new Error('Invalid color component value, must be between 0 and 255');
+    }
+
+    return `#${toHex(red)}${toHex(green)}${toHex(blue)}`.toUpperCase();
+  }
 </script>
 
 <div class="mt-5 sliders">
@@ -13,7 +26,8 @@
   <Slider color="Blue"  bind:value={blue}/>
 </div>
 
-<div class="mt-5 color-box" style="background-color:rgb({red}, {green}, {blue})"></div>
+<h3 class="mt-5">{rgbToHex(red, green, blue)}</h3>
+<div class="color-box" style="background-color:rgb({red}, {green}, {blue})"></div>
 
 <style>
   .sliders {
