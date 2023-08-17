da2a088af4090ed357b2b112e6311eae54b3f3a0
Andrew Smith <andrew.smith@wwinc.com>
2023-08-17 13:11:13 -0500

    Added a red color slider.

--- src/lib/components/color-picker/color-picker.svelte
+++ src/lib/components/color-picker/color-picker.svelte
@@ -1,9 +1,27 @@
-<div class="mt-5"></div>
+
+<div class="mt-5 sliders">
+  <div class="row">
+    <div class="col-sm-12">
+      <label for="red">Red</label>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-12">
+      <input class="form-range" type="range" name="red" id="red">
+    </div>
+  </div>
+</div>
+
+<div class="mt-5 color-box"></div>
 
 <style>
-  div {
-    height:       400px;
-    width:        400px;
+  .sliders {
+    width: 600px;
+  }
+
+  .color-box {
+    height:       600px;
+    width:        600px;
     border-style: dashed;
     border-width: 4px;
     border-color: black;
