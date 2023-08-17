3cb40082832895b86f335ffa4158be59d25c80f1
Andrew Smith <andrew.smith@wwinc.com>
2023-08-17 12:51:05 -0500

    Added a basic layout with a nav bar.

---
+++ src/lib/components/nav.svelte
@@ -0,0 +1,41 @@
+<script>
+
+  import { page } from '$app/stores';
+
+  let menuVisible = false;
+
+  function toggleMenu() {
+    menuVisible = !menuVisible;
+  }
+
+</script>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Book Lover</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+      on:click={toggleMenu}
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav" class:show={menuVisible}>
+      <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class:active={$page.url.pathname === '/'} class="nav-link" href="/">Home</a>
+          </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<style>
+  .nav-item > span {
+    cursor: pointer;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import Nav from '$lib/components/nav.svelte';
+</script>
+
+<Nav/>
+
+<main class="container">
+  <slot/>
+</main>
