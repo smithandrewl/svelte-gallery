32806a7e00f2dd2e107d55977e6c8c1fe4a0dc8e
Andrew Smith <andrew.smith@wwinc.com>
2023-08-18 07:46:38 -0500

    Removed fancy border.

--- src/lib/components/color-picker/color-picker.svelte
+++ src/lib/components/color-picker/color-picker.svelte
@@ -6,15 +6,6 @@
   let green = 127;
   let blue  = 127;
 
-  function lighter(value: number): number {
-    let brighter = value * 1.5;
-    return Math.min(brighter, 255);
-  }
-
-  function darker(value: number): number {
-    return value / 2;
-  }
-
   function rgbToHex(red: number, green: number, blue: number): string {
     const toHex = (value: number): string => {
       const hex = value.toString(16);
@@ -27,9 +18,6 @@
 
     return `#${toHex(red)}${toHex(green)}${toHex(blue)}`.toUpperCase();
   }
-
-  $: lighterColor = `rgb(${lighter(red)}, ${lighter(green)}, ${lighter(blue)})`;
-  $: darkerColor = `rgb(${darker(red)}, ${darker(green)}, ${darker(blue)}`;
 </script>
 
 <div class="mt-5 sliders">
@@ -39,7 +27,7 @@
 </div>
 
 <h3 class="mt-5">{rgbToHex(red, green, blue)}</h3>
-<div class="color-box" style="background-color:rgb({red}, {green}, {blue}); border-left-color: {lighterColor}; border-top-color: {lighterColor}; border-right-color: {darkerColor}; border-bottom-color: {darkerColor}"></div>
+<div class="color-box" style="background-color:rgb({red}, {green}, {blue});"></div>
 
 <style>
   .sliders {
@@ -51,6 +39,5 @@
     width:        400px;
     border-style: solid;
     border-width: 10px;
-
   }
 </style>
