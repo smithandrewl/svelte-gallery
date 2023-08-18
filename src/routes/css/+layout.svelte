435726c87261b9c7776a6e3258a31f65c38bdb7c
Andrew Smith <andrew.smith@wwinc.com>
2023-08-18 09:16:43 -0500

    Added a place-holder shadow generator page.

--- src/routes/css/+layout.svelte
+++ src/routes/css/+layout.svelte
@@ -1,8 +1,12 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
 <h1>CSS Tools</h1>
 
 <nav class="nav mb-5">
-  <a class="nav-link active" href="/css/">Color Picker</a>
-  <a class="nav-link" href="/css/other">Other</a>
+  <a class:active={$page.url.pathname === "/css"} class="nav-link active" href="/css/">Color Picker</a>
+  <a class:active={$page.url.pathname === "/css/shadow-generator"} class="nav-link" href="/css/shadow-generator">Shadow Generator</a>
 </nav>
 
 <slot/>
