6274b99b2d7c6e886d73405d2f26cf6237bdb21c
Andrew Smith <andrew.smith@wwinc.com>
2023-08-21 13:09:41 -0500

    Added a range control for the angle.

--- src/lib/components/gradient-generator/gradient-generator.svelte
+++ src/lib/components/gradient-generator/gradient-generator.svelte
@@ -4,6 +4,8 @@
 
   let startColor = "#000000";
   let endColor = "#000000";
+  let angle = 0;
+
 </script>
 <div class="row">
   <div class="col-sm-4">
@@ -13,12 +15,18 @@
   </div>
   <div class="col-sm-4">
     <!-- Gradient controls -->
-    <div class="row">
+    <div class="row mb-5">
       <div class="d-flex align-content-center">
         <input class="form-check-input me-1" type="radio" name="gradient-type" id="linear" value="linear">
         <label class="form-check-label me-5" for="linear">Linear</label>
         <input class="form-check-input me-1" type="radio" name="gradient-type" id="radial" value="radial">
         <label class="from-check-label" for="radial">Radial</label>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <label for="angle">Angle: {angle}</label>
+        <input class="form-range" type="range" id="angle" bind:value={angle}>
       </div>
     </div>
   </div>
