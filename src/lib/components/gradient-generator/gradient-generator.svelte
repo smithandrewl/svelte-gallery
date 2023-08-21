7b4420ee77f68a8d7dd9b6f6772f9a6c8a27d448
Andrew Smith <andrew.smith@wwinc.com>
2023-08-21 12:50:48 -0500

    Created a color-picker component.

--- src/lib/components/gradient-generator/gradient-generator.svelte
+++ src/lib/components/gradient-generator/gradient-generator.svelte
@@ -1,30 +1,15 @@
 <script lang="ts">
+
+  import ColorPicker from '$lib/components/misc/color-picker.svelte';
+
   let startColor = "#000000";
   let endColor = "#000000";
 </script>
 <div class="row">
   <div class="col-sm-4">
     <!-- Colors and presets -->
-
-    <div class="row">
-      <div class="col-sm-12 mb-4">
-        <label for="start-color" class="form-label">Start Color</label>
-        <div class="d-flex align-items-center">
-          <input class="form-control form-control-color me-2" type="color" id="start-color" bind:value={startColor}>
-          <div>{startColor}</div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-12">
-        <label for="end-color" class="form-label">End Color</label>
-        <div class="d-flex align-items-center">
-          <input class="form-control form-control-color me-2" type="color" id="end-color" bind:value={endColor}>
-          <div>{endColor}</div>
-        </div>
-      </div>
-    </div>
+        <ColorPicker label="Start Color" bind:color={startColor}/>
+        <ColorPicker label="End Color" bind:color={endColor}/>
   </div>
   <div class="col-sm-4">
     <!-- Gradient controls -->
---
+++ src/lib/components/misc/color-picker.svelte
@@ -0,0 +1,22 @@
+
+<script lang="ts">
+  import {makeKebab} from '$lib/util';
+
+  export let label: string;
+  export let color: string;
+
+  $: id = makeKebab(label) + "-color"
+</script>
+
+<div class="mb-4">
+  <label for={id} class="form-label">{label}</label>
+  <div class="d-flex align-items-center">
+    <input
+      class      = "form-control form-control-color me-2"
+      type       = "color"
+      id         = {id}
+      bind:value = {color}
+    >
+    <div>{color}</div>
+  </div>
+</div>
