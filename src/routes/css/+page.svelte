3789369dcb3c5b65361e1d4c0bc1aade8a083e9f
Andrew Smith <andrew.smith@wwinc.com>
2023-08-23 11:39:49 -0500

    Added a background image for the color picker section.

--- src/routes/css/+page.svelte
+++ src/routes/css/+page.svelte
@@ -3,3 +3,16 @@ import ColorPicker from "$lib/components/color-picker/color-picker.svelte";
 </script>
 
 <ColorPicker></ColorPicker>
+
+<svelte:head>
+  <style>
+    body {
+      background: url(/colors.png) no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+    }
+  </style>
+</svelte:head>
+
