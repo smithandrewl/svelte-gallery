5053c3462e58eb5fbb31d6abec825f8765c21736
Andrew Smith <andrew.smith@wwinc.com>
2023-09-07 13:32:26 -0500

    Added a card to make the gradient controls easier to see

--- src/lib/components/gradient-generator/gradient-generator.svelte
+++ src/lib/components/gradient-generator/gradient-generator.svelte
@@ -18,53 +18,58 @@
 </script>
 <div class="row">
   <div class="col-sm-8">
-    <div class="row">
-      <div class="col-sm-6">
-        <!-- Colors and presets -->
-        <ColorPicker label="Start Color" bind:color={$store.startColor} />
-        <ColorPicker label="End Color"   bind:color={$store.endColor}/>
-      </div>
-      <div class="col-sm-6">
-        <!-- Gradient controls -->
-        <div class="row mb-5">
-          <div class="d-flex align-content-center">
-            <input
-              class      = "form-check-input me-1"
-              type       = "radio"
-              name       = "gradient-type"
-              id         = "linear"
-              value      = "linear"
-              bind:group = {$store.type}
-            >
-
-            <label class="form-check-label me-5" for="linear">Linear</label>
-
-            <input
-              class      = "form-check-input me-1"
-              type       = "radio"
-              name       = "gradient-type"
-              id         = "radial"
-              bind:group = {$store.type}
-              value      = "radial"
-            >
-
-            <label class="from-check-label" for="radial">Radial</label>
-          </div>
-        </div>
+    <div class="card">
+      <div class="card-body">
         <div class="row">
-          <div class="col-sm-12">
-            <label for="angle">Angle: {$store.angle}</label>
-            <input
-              class      = "form-range"
-              type       = "range"
-              id         = "angle"
-              bind:value = {$store.angle}
-              disabled   = {angleDisabled}
-            >
+          <div class="col-sm-6">
+            <!-- Colors and presets -->
+            <ColorPicker label="Start Color" bind:color={$store.startColor} />
+            <ColorPicker label="End Color"   bind:color={$store.endColor}/>
+          </div>
+          <div class="col-sm-6">
+            <!-- Gradient controls -->
+            <div class="row mb-5">
+              <div class="d-flex align-content-center">
+                <input
+                  class      = "form-check-input me-1"
+                  type       = "radio"
+                  name       = "gradient-type"
+                  id         = "linear"
+                  value      = "linear"
+                  bind:group = {$store.type}
+                >
+
+                <label class="form-check-label me-5" for="linear">Linear</label>
+
+                <input
+                  class      = "form-check-input me-1"
+                  type       = "radio"
+                  name       = "gradient-type"
+                  id         = "radial"
+                  bind:group = {$store.type}
+                  value      = "radial"
+                >
+
+                <label class="from-check-label" for="radial">Radial</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-12">
+                <label for="angle">Angle: {$store.angle}</label>
+                <input
+                  class      = "form-range"
+                  type       = "range"
+                  id         = "angle"
+                  bind:value = {$store.angle}
+                  disabled   = {angleDisabled}
+                >
+              </div>
+            </div>
           </div>
         </div>
+        </div>
       </div>
-    </div>
+
     <div class="row">
       <h2>Css</h2>
       <div id="generatedCss">
