be1198e3b80000d005d9bcbc5fd58fe9da0f0806
Andrew Smith <andrew.smith@wwinc.com>
2023-09-08 15:09:28 -0500

    Created an empty layout based on the gradient generator layout.

--- src/lib/components/border-radius-generator/border-radius-generator.svelte
+++ src/lib/components/border-radius-generator/border-radius-generator.svelte
@@ -1,1 +1,31 @@
-Border Radius Generator
+<script>
+import CssBox from "$lib/components/css-box/css-box.svelte";
+import ColorPicker from "$lib/components/misc/color-picker.svelte";
+import GradientBox
+  from "$lib/components/gradient-generator/gradient-box.svelte";
+</script>
+
+<div class="row">
+  <div class="col-sm-8">
+    <div class="row">
+      <div class="card">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-sm-6">
+            </div>
+            <div class="col-sm-6">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <CssBox css=""/>
+    </div>
+  </div>
+  <div class="col-sm-4">
+
+  </div>
+</div>
