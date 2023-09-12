2441b9a7a3f30316a7443d313cf5d9d0ae06eb32
Andrew Smith <andrew.smith@wwinc.com>
2023-09-12 13:04:40 -0500

    Started to flesh out the UI some more.

--- src/lib/components/color-conversion/color-conversion.svelte
+++ src/lib/components/color-conversion/color-conversion.svelte
@@ -9,7 +9,38 @@
 </script>
 
 <div class="row">
-  <div class="col-sm-6"></div>
+  <div class="col-sm-6">
+    <!-- Hex -->
+    <div class="row">
+      <div class="col-sm-2">
+        <input id="rdioHex" type="radio" name="type"/>
+        <label for="rdioHex">Hex</label>
+      </div>
+      <div class="col-sm-8"><input type="text"/></div>
+    </div>
+
+    <!-- RGB -->
+    <div class="row">
+      <div class="col-sm-2">
+        <input id="rdioRgb" type="radio" name="type"/>
+        <label for="rdioRgb">RGB</label>
+      </div>
+      <div class="col-sm-8">
+        <label for="numRed">Red: </label> <input type="number" size="3"/>
+        <label for="numGreen">Green: </label> <input type="number" size="3"/>
+        <label for="numBlue">Blue: </label><input type="number" size="3"/>
+      </div>
+    </div>
+
+    <!-- HSL -->
+    <div class="row">
+      <div class="col-sm-12">
+        <input id="rdioHsl" type="radio" name="type"/>
+        <label for="rdioHsl">HSL</label>
+      </div>
+    </div>
+
+  </div>
   <div class="col-sm-6">
     <h3>Color Details:</h3>
     <pre>
