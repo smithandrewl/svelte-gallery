60c94ecd89b7920e46c39a90b6701520545e8c82
Andrew Smith <andrew.smith@wwinc.com>
2023-09-12 12:49:26 -0500

    Created a proper component for the color conversion page.

--- src/routes/color/convert/+page.svelte
+++ src/routes/color/convert/+page.svelte
@@ -1,15 +1,5 @@
-<script lang="ts">
-
-  import {createColorConversionStore, createColorInfoStore} from "$lib/stores/color-conversion-store.ts";
-
-  let colorConversionStore = createColorConversionStore();
-
-  let colorInfoStore = createColorInfoStore(colorConversionStore);
-
+<script>
+  import ColorConversion from "$lib/components/color-conversion/color-conversion.svelte";
 </script>
 
-<pre>
-  Hex: {$colorInfoStore.hex}
-  RGB: {$colorInfoStore.rgb.red}, {$colorInfoStore.rgb.green}, {$colorInfoStore.rgb.blue}
-  HSL: {$colorInfoStore.hsl.h}, {$colorInfoStore.hsl.s}, {$colorInfoStore.hsl.l}
-</pre>
+<ColorConversion/>
