5ea84fa31f6bd1c2c08efd1a2236661153f74b24
Andrew Smith <andrew.smith@wwinc.com>
2023-09-13 09:31:21 -0500

    Created folders and pages for new routes.

---
+++ src/routes/color/convert/+layout.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+
+  import {createColorConversionStore, createColorInfoStore, createColorInfoCSSStore} from "$lib/stores/color-conversion-store.ts";
+
+  let colorConversionStore = createColorConversionStore();
+
+  let colorInfoStore = createColorInfoStore(colorConversionStore);
+  let css = createColorInfoCSSStore(colorInfoStore);
+
+  import CssBox from '$lib/components/css-box/css-box.svelte';
+</script>
+
+<style>
+  .form-control {
+    width: 5em;
+  }
+</style>
+
+<div class="row">
+  <div class="col-sm-6">
+    <div class="row d-flex justify-content-center">
+      <div class="col-sm-2"><input id="rdioHex" type="radio" name="type" checked="checked"/>
+        <label for="rdioHex">Hex</label></div>
+      <div class="col-sm-2">
+        <input id="rdioRgb" type="radio" name="type"/>
+        <label for="rdioRgb">RGB</label></div>
+      <div class="col-sm-2">
+        <input id="rdioHsl" type="radio" name="type"/>
+        <label for="rdioHsl">HSL</label></div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-12">
+        <slot/>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="row">
+      <h3>Color Details:</h3>
+
+      <div class="row">
+        <div class="col-sm-12">
+          <b>Hex:</b> {$colorInfoStore.hex}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <b>RGB:</b> {$colorInfoStore.rgb.red}, {$colorInfoStore.rgb.green}, {$colorInfoStore.rgb.blue}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <b>HSL:</b> {$colorInfoStore.hsl.h}&deg;, {$colorInfoStore.hsl.s}&percnt;, {$colorInfoStore.hsl.l}&percnt;
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <CssBox css={$css}/>
+    </div>
+  </div>
+</div>
+
+
--- src/routes/color/convert/+page.svelte
+++ src/routes/color/convert/+page.svelte
@@ -1,5 +1,1 @@
-<script>
-  import ColorConversion from "$lib/components/color-conversion/color-conversion.svelte";
-</script>
-
-<ColorConversion/>
+Hex
